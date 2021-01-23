386c186ca279da852a4c78fd5cb66259d41e35e6
swapnilshinde123 <swapnil6901@gmail.com>
2021-01-23 21:04:32 +0530

    IMDb

---
+++ IMDb/src/components/elements/Header/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Header.css';
+
+const Header = () => (
+  <div className="rmdb-header">
+    <div className="rmdb-header-content">
+      <Link to="/">
+        <img src="/images/reactMovie_logo.png" alt="rmdb-logo" />
+       
+      </Link>
+      <img className="rmdb-tmdb-logo" src="/images/tmdb_logo.png" alt="tmdb-logo" />
+    </div>
+  </div>
+)
+
+export default Header;
